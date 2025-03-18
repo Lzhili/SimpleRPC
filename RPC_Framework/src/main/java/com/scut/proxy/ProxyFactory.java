7f20ef04f538601e19de144776dca6b37242dba4
laizhili <969414747@qq.com>
2025-03-18 14:58:57 +0800

    简单模拟服务容错和服务重试

--- RPC_Framework/src/main/java/com/scut/proxy/ProxyFactory.java
+++ RPC_Framework/src/main/java/com/scut/proxy/ProxyFactory.java
@@ -9,6 +9,7 @@ import com.scut.register.MapRemoteRegister;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProxyFactory {
@@ -26,12 +27,28 @@ public class ProxyFactory {
                 //服务发现
                 List<URL> list = MapRemoteRegister.get(interfaceClass.getName());
 
-                //负载均衡
-                URL url = LoadBalance.random(list);
+                String result = null;
 
-                //服务调用
-                String result = httpClient.send(url.getHostname(), url.getPort(), invocation);
+                int maxRetries = 3; //服务重试最多3次
+                List<URL> invokedUrls = new ArrayList<>(); //记录已经调用过的url
 
+                while (maxRetries > 0) { //服务重试
+                    //负载均衡
+                    list.remove(invokedUrls);
+                    URL url = LoadBalance.random(list);
+                    invokedUrls.add(url);
+
+                    try {
+                        //服务调用
+                        result = httpClient.send(url.getHostname(), url.getPort(), invocation);
+                        if(result != null) break;
+                    } catch (Exception e) {
+                        System.out.println("try failed"); //尝试失败
+                        if(--maxRetries != 0) continue;
+                        // errorCallback，容错逻辑
+                        return "Service not found, please start the provider first!";
+                    }
+                }
                 return result;
             }
         });
